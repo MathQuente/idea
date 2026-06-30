e5eeab6aa32d8ab7025a777a6e424033e22d5c2e
Matheus Silva <math.de.sousa.silva@gmail.com>
2026-06-30 18:43:51 -0300

    Flash Messaging lesson

--- resources/views/components/layout/layout.blade.php
+++ resources/views/components/layout/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    @vite(['resources/css/app.css'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="bg-background text-foreground">
@@ -15,6 +15,13 @@
     <main class="max-w-7x1 mx-auto px-6 py-10">
         {{ $slot }}
     </main>
+
+    @session('success')
+        <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 2000)" x-show="show" x-transition.opacity.duration.300ms
+            class="bg-primary px-4 py-3 absolute bottom-4 right-4 rounded-1g">
+            {{ $value }}
+        </div>
+    @endsession
 </body>
 
 </html>
