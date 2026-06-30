2a48b0c3795bd7278cfe70da84848fe9e64c2271
Matheus Silva <math.de.sousa.silva@gmail.com>
2026-06-29 22:15:45 -0300

    Tailwind Theme Setup and Initial UI lesson

---
+++ resources/views/components/layout/nav.blade.php
@@ -0,0 +1,20 @@
+<nav class="border-b border-border px-6">
+    <div class="max-w-7xl mx-auto h-16 flex items-center justify-between">
+        <div>
+            <a href="">Idea</a>
+        </div>
+
+        <div class="flex gap-x-5 items-center">
+            @auth
+                <form method="POST" action="/logout">
+                    @csrf
+                    <button>Log Out</button>
+                </form>
+            @endauth
+            @guest
+                <a href="/login">Log In</a>
+                <a href="/register" class="btn">Register</a>
+            @endguest
+        </div>
+    </div>
+</nav>
